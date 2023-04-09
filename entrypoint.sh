b8243ddff4878a47a4d7aead653d28ea7597e61a
Evgeni Golov <evgeni@golov.de>
2023-04-09 16:54:25 +0200

    fix parameter passing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,12 @@
 if [ ! -d /var/lib/docker ]; then
   ORIG_CONTAINER=$(docker ps --latest --quiet)
   ORIG_IMAGE=$(docker inspect ${ORIG_CONTAINER} --format '{{.Config.Image}}')
-  docker run --workdir /github/workspace --rm  $(docker inspect ${ORIG_CONTAINER} --format '-v {{join .HostConfig.Binds " -v "}}') -v "/var/lib/docker:/var/lib/docker" ${ORIG_IMAGE} $@
+  docker run --workdir /github/workspace --rm  $(docker inspect ${ORIG_CONTAINER} --format '-v {{join .HostConfig.Binds " -v "}}') -v "/var/lib/docker:/var/lib/docker" ${ORIG_IMAGE} "$1" "$2" "$3"
 else
-  piuparts $1 --docker-image $2 --fake-essential-packages $3
+  if [ -n "$3" ]; then
+    ARGS="--fake-essential-packages $3"
+  else
+    ARGS=""
+  fi
+  piuparts $1 --docker-image $2 ${ARGS}
 fi
