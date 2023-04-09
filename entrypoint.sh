14e846daf33831b018ddd902cb981a883d644f77
Evgeni Golov <evgeni@golov.de>
2023-04-09 17:11:52 +0200

    move orig binds into own line

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 if [ ! -d /var/lib/docker ]; then
   ORIG_CONTAINER=$(docker ps --latest --quiet)
   ORIG_IMAGE=$(docker inspect ${ORIG_CONTAINER} --format '{{.Config.Image}}')
-  docker run --workdir /github/workspace --rm  $(docker inspect ${ORIG_CONTAINER} --format '-v {{join .HostConfig.Binds " -v "}}') -v "/var/lib/docker:/var/lib/docker" ${ORIG_IMAGE} "$1" "$2" "$3"
+  ORIG_BINDS=$(docker inspect ${ORIG_CONTAINER} --format '-v {{join .HostConfig.Binds " -v "}}')
+  docker run --workdir /github/workspace --rm ${ORIG_BINDS} -v "/var/lib/docker:/var/lib/docker" ${ORIG_IMAGE} "$1" "$2" "$3"
 else
   if [ -n "$3" ]; then
     ARGS="--fake-essential-packages $3"
